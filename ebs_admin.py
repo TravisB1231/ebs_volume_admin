154c57c4924011cad503efde9957135c64e0428d
Travis Brown <travisrbrown1231@gmail.com>
2022-12-05 16:19:22 -0500

    Started looping logic. Error on validate_response_code()

--- ebs_admin.py
+++ ebs_admin.py
@@ -1,19 +1,19 @@
-import sys
 import json
 
 import aws_auth
 from ebs_volume import EBSVolume
 
-REGION = "us-east-2"
+REGIONS = [ "ap-northeast-1", "ap-northeast-2", "ap-northeast-3", "ap-south-1", "ap-southeast-1", "ap-southeast-2", "eu-central-1", "eu-west-1", "eu-west-2", "eu-west-3", "eu-north-1", "us-east-1", "us-east-2", "us-west-1", "us-west-2"]
 
 def write_output_json(resp:dict) -> None:
     """Useful for testing to get raw boto3 output.
     Writes to olympus_output.json in json format."""
-    with open("output.json", 'w', encoding="UTF-8") as outfile:
+    with open("output.json", 'a', encoding="UTF-8") as outfile:
         outfile.write(json.dumps(resp, default=str))
 
 def upgrade_to_gp3(vols:list):
-    client = aws_auth.botosesh.client('ec2', region_name=REGION)
+    for i in enumerate(REGIONS, 0):
+        client = aws_auth.botosesh.client('ec2', region_name=REGIONS[i[0]])
     for volume in vols:
         if volume.volume_type == "gp2":
             client.modify_volume(
@@ -21,7 +21,6 @@ def upgrade_to_gp3(vols:list):
                 VolumeType='gp3'
             )
             print(f"{volume.id} modified to gp3.")
-
             
 def enumerate_volumes(res:dict):
     _raw_response_data = res["Volumes"]
@@ -32,11 +31,12 @@ def enumerate_volumes(res:dict):
     return volumes
 
 def main():
-    response = aws_auth.botosesh.client('ec2', region_name=REGION).describe_volumes()
-    if aws_auth.validate_response_code(response):
-        write_output_json(response)
-        ebs_volumes = enumerate_volumes(response)
-        upgrade_to_gp3(ebs_volumes)
+    for i in enumerate(REGIONS, 0):
+        response = aws_auth.botosesh.client('ec2', region_name=REGIONS[i[0]])
+        if aws_auth.validate_response_code(response):
+            write_output_json(response)
+    # ebs_volumes = enumerate_volumes(response)
+    # upgrade_to_gp3(ebs_volumes)
         
 if __name__ == "__main__":
     main()
